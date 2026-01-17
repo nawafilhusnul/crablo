32a89c857a6f0d69ef00e9e9f1ccc498f660b774
nawafilhusnul <nawafilhusnul@gmail.com>
2026-01-17 17:31:19 +0700

    Implement pathfinding algorithm and update target selection logic

--- crablo/src/main.rs
+++ crablo/src/main.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use macroquad::prelude::*;
+use std::collections::VecDeque;
 
 const MAP: usize = 20;
 const T_SIZE: (f32, f32) = (32., 16.);
@@ -32,6 +33,48 @@ fn to_tile(sx: f32, sy: f32, cam: (f32, f32)) -> (usize, usize) {
         ((ax / T_SIZE.0 + ay / T_SIZE.1) / 2.) as usize,
         ((ay / T_SIZE.1 - ax / T_SIZE.0) / 2.) as usize,
     )
+}
+
+// Pathfinding algo
+fn bfs(
+    map: &[[Tile; MAP]; MAP],
+    start: (usize, usize),
+    goal: (usize, usize),
+) -> Vec<(usize, usize)> {
+    let mut q = VecDeque::from([start]);
+    let mut visited = [[false; MAP]; MAP];
+    visited[start.1][start.0] = true;
+
+    let mut parent = [[None; MAP]; MAP];
+
+    while let Some(curr) = q.pop_front() {
+        if curr == goal {
+            let mut path = vec![];
+            let mut c = goal;
+            while c != start {
+                path.push(c);
+
+                c = parent[c.1][c.0].unwrap()
+            }
+
+            path.reverse();
+
+            return path;
+        }
+
+        // check the close
+        for (dx, dy) in [(0, -1), (0, 1), (-1, 0), (1, 0)] {
+            let (nx, ny) = ((curr.0 as i32 + dx) as usize, (curr.1 as i32 + dy) as usize);
+
+            if nx < MAP && ny < MAP && !visited[ny][nx] && map[ny][nx] == Tile::Floor {
+                visited[ny][nx] = true;
+                parent[ny][nx] = Some(curr);
+                q.push_back((nx, ny));
+            }
+        }
+    }
+
+    vec![]
 }
 
 // draw hero and monsters
@@ -98,7 +141,7 @@ struct Game {
     cam: (f32, f32),
     px: usize,
     py: usize,
-    target: Option<(usize, usize)>,
+    path: Vec<(usize, usize)>,
 }
 
 impl Game {
@@ -122,7 +165,7 @@ impl Game {
             cam: (screen_width() / 2., 50.),
             px: 2,
             py: 2,
-            target: None,
+            path: vec![],
         }
     }
 
@@ -138,8 +181,8 @@ impl Game {
             let (tx, ty) = to_tile(mx, my, self.cam);
 
             // check if the click is inside the map bounds
-            if tx < MAP && ty < MAP {
-                self.target = Some((tx, ty));
+            if tx < MAP && ty < MAP && self.map[ty][tx] == Tile::Floor {
+                self.path = bfs(&self.map, (self.px, self.py), (tx, ty))
             }
         }
 
@@ -158,11 +201,10 @@ impl Game {
             }
         }
 
-        // draw the target
-        if let Some((tx, ty)) = self.target {
-            let (sx, sy) = to_screen(tx, ty, self.cam);
-
-            draw_circle(sx, sy + 16., 6., RED);
+        // draw the path
+        for (px, py) in &self.path {
+            let (sx, sy) = to_screen(*px, *py, self.cam);
+            draw_circle(sx, sy + 16., 4., GOLD);
         }
 
         // draw the player
